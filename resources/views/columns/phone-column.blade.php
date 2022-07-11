f00eefde75a6f892bae4da555f2dbae123313d46
Oleksandr Petrovskyi <sashalenz@gmail.com>
2022-07-11 16:37:40 +0300

    fix

--- resources/views/columns/phone-column.blade.php
+++ resources/views/columns/phone-column.blade.php
@@ -1,3 +1,5 @@
-<a href="tel:{{ $data->formatE164() }}" class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5">
-    {{ $data->formatInternational() }}
-</a>
+@if($data)
+    <a href="tel:{{ $data->formatE164() }}" class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5">
+        {{ $data->formatInternational() }}
+    </a>
+@endif
